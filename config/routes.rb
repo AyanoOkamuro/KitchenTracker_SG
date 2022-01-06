92dd7573998821d2407a57e0e17b627e40b8b8be
AyanoOkamuro <omochi185@gmail.com>
2022-01-06 12:06:42 +0900

    プロフィール編集後の遷移先をプロフィールページに

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  devise_for :kitchencars
+  devise_for :kitchencars, controllers: {
+    sessions: 'kitchencars/sessions',
+    registrations: 'kitchencars/registrations',
+  }
   resources :kitchencars, only: [:show]
   resources :microposts, only: [:index, :create, :edit, :update, :destroy]
 end
